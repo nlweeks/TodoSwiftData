7381d90510a3bbf3ca0986ef290146d4bc46f9ae
nlweeks <noah.weeks@gmail.com>
2025-04-09 12:47:51 -0400

    Initial Commit

---
+++ TodoSwiftData/TodoSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftDataApp.swift
+//  TodoSwiftData
+//
+//  Created by Noah Weeks on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
